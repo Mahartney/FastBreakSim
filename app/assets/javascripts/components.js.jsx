e5bca83dd43fdf502f8908e458f4ebfb06a8661f
Davis Green <davismgreen@gmail.com>
2016-03-04 15:08:07 -0500

    adds test react component

--- app/assets/javascripts/components.js.jsx
+++ app/assets/javascripts/components.js.jsx
@@ -1,11 +1,11 @@
 //= require_tree ./components
 
-var HelloMessage = React.createClass({
-  render: function() {
+class HelloMessage extends React.Component {
+  render() {
     return (
       <div className="commentBox">
         <h1>Comments {this.props.name} </h1>
       </div>
     );
   }
-});
+};
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
 
   config.react.jsx_transform_options = {
     blacklist: ['spec.functionName', 'validation.react', 'strict'], # default options
-    optional: ["transformerName"],  # pass extra babel options
+#    optional: ["transformerName"],  # pass extra babel options
     whitelist: ["useStrict"] # even more options
   }
-  
+
 end
